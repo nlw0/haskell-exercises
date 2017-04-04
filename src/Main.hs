88e8ea8c06b0f1a0075bb5581ab7bbfe13b085a9
nlw0 <nwerneck@gmail.com>
2017-04-04 22:23:27 +0200

    still have to modify to guarantee the smallest id... (seem to be a problem in hackerrank)

--- src/Main.hs
+++ src/Main.hs
@@ -20,13 +20,15 @@ f :: [String] -> [String]
 f ll = map (show . getMostCommonBird . parseBirds) $ tail ll
 
 parseBirds :: String -> [Int]
-parseBirds xx = map read $ words xx :: [Int]
+parseBirds xx = map read $ words xx
 
-getMostCommonBird::[Int] -> [Int]
-getMostCommonBird bb = sort bb
+getMostCommonBird bb = snd themax
+    where count = birdCount 1 (head bb) (tail bb)
+          themax = foldl1 (\(na, a) (nb, b) -> if na>=nb then (na, a) else (nb, b)) count
 
---countBirds :: [Int] -> [(Int, Int)]
---countBirds bb = head bb
-
-
-
+birdCount::Int -> Int -> [Int] -> [(Int,Int)]
+birdCount acc bird [] = (acc, bird) : []
+birdCount acc bird (newbird:bb) =
+  if bird == newbird
+    then birdCount (acc + 1) bird bb
+    else (acc, bird) : birdCount 1 newbird bb
